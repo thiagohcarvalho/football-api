8f523fc016ad26bb500c0ffd36b1206f1998213e
Thiago Carvalho <thiagohcarvalho10@gmail.com>
2025-10-05 16:30:11 -0300

    first commit

---
+++ src/main/java/io/github/thiagohcarvalho/football_api/FootballApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.thiagohcarvalho.football_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/thiagohcarvalho/football_api/FootballApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.thiagohcarvalho.football_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
