dc88d77e43ca11ea0193db7323942af858bc43a8
Thiago Carvalho <thiagohcarvalho10@gmail.com>
2025-11-22 02:20:13 -0300

    finished service

--- src/main/java/io/github/thiagohcarvalho/football_api/service/PlayerService.java
+++ src/main/java/io/github/thiagohcarvalho/football_api/service/PlayerService.java
@@ -2,10 +2,14 @@ package io.github.thiagohcarvalho.football_api.service;
 
 import io.github.thiagohcarvalho.football_api.entity.Player;
 import io.github.thiagohcarvalho.football_api.repository.PlayerRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 // ideia: usar filter para diminuir a quantidade de chamadas
 @Component
@@ -40,5 +44,31 @@ public class PlayerService {
 
     public List<Player> getPlayersFromTeamAndPosition(String teamName, String position) {
         return playerRepository.findByTeam_nameAndPosition(teamName, position);
+    }
+
+    public Player addPlayer(Player player) {
+        return playerRepository.save(player);
+    }
+
+    public Player updatePlayer(Player updatedPlayer) {
+        Optional<Player> existingPlayer = playerRepository.findByName(updatedPlayer.getName());
+
+        if (existingPlayer.isPresent()) {
+            Player playerToUpdate = existingPlayer.get();
+            playerToUpdate.setName(updatedPlayer.getName());
+            playerToUpdate.setTeam_name(updatedPlayer.getTeam_name());
+            playerToUpdate.setPosition(updatedPlayer.getPosition());
+            playerToUpdate.setNation(updatedPlayer.getNation());
+
+            return playerRepository.save(playerToUpdate);
+        }
+
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Player not found");
+        // implement custom exceptions - NotFound
+    }
+
+    @Transactional
+    public void deletePlayer(String playerName) {
+        playerRepository.deleteByName(playerName);
     }
 }
